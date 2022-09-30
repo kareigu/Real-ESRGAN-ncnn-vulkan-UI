181c7fd9dd5610983be857266ac6febc768ffb1b
karei <eetu.kivelae@gmail.com>
2022-09-30 20:51:15 +0300

    Implemented running the CLI

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,41 +1,112 @@
 #include "mainwindow.h"
 #include <QVBoxLayout>
 #include <QHBoxLayout>
+#include <QCoreApplication>
+#include <QFile>
+#include <QFileInfo>
+#include <QDir>
 #include <QLineEdit>
-#include <QPushButton>
+#include <QThread>
 
 MainWindow::MainWindow(QWidget *parent)
   : QMainWindow(parent) {
 
-  setMinimumSize(QSize(650, 300));
-  setMaximumSize(QSize(750, 300));
+  setMinimumSize(QSize(650, 400));
+  setMaximumSize(QSize(750, 400));
   setWindowTitle("realesrgan-nccn-vulkan");
 
   m_main_view = new QWidget(this);
 
-  m_path_selects = new QGroupBox(m_main_view);
+  m_main_controls = new QGroupBox(m_main_view);
+  m_main_controls->setLayout(new QVBoxLayout);
+
+  m_path_selects = new QWidget(m_main_controls);
   m_path_selects->setLayout(new QVBoxLayout);
+
   m_input_select = new PathPicker(tr("Input"));
   m_output_select = new PathPicker(tr("Output"));
   m_output_select->set_folder_mode();
   m_input_select->set_select_callback([&](const QString& path) {
-    auto separator = path.lastIndexOf("/");
-    QString folder_path = path.left(separator);
-    QStringList filename = path.right(separator).split(".");
-    m_output_select->set_path(
-      QString("%1/%2")
-        .arg(folder_path)
-        .arg(
-          QString("%1%2.%3")
-            .arg(filename[0])
-            .arg("_up")
-            .arg(filename[1])
-        )
-    );
+    QFileInfo file_info(path);
+    QDir folder = file_info.dir();
+    QString filename = QString("%1_up.%2")
+      .arg(file_info.baseName())
+      .arg(file_info.completeSuffix());
+    m_output_select->set_path(folder.filePath(filename));
   });
 
   m_path_selects->layout()->addWidget(m_input_select);
   m_path_selects->layout()->addWidget(m_output_select);
+
+  m_main_buttons = new QWidget(m_main_controls);
+  m_main_buttons->setLayout(new QHBoxLayout);
+  m_main_buttons->layout()->setAlignment(Qt::AlignCenter);
+
+  QSize button_size(60, 25);
+  m_start_button = new QPushButton(m_main_buttons);
+  m_start_button->setText("&Start");
+  m_start_button->setFixedSize(button_size);
+  connect(m_start_button, &QPushButton::released, this, [&]() {
+    if (m_cli) {
+      debugln("Already running");
+      return;
+    }
+    m_cli = new QProcess();
+
+#ifdef WIN32
+    QString program_path(QCoreApplication::applicationDirPath() + "/cli/realesrgan-ncnn-vulkan.exe");
+#else
+    QString program_path("cli/realesrgan-ncnn-vulkan");
+#endif
+
+    QStringList arguments;
+    arguments << "-i" << m_input_select->path() << "-o" << m_output_select->path();
+
+    QFile program(program_path);
+
+    if (!program.exists()) {
+      debugln("CLI doesn't exist");
+      debugln(program_path);
+      return;
+    }
+
+    m_cli->setProcessChannelMode(QProcess::ProcessChannelMode::MergedChannels);
+
+    connect(m_cli, &QProcess::readyRead, this, [&]() {
+      auto output = QString(m_cli->readAll()).split("\n");
+      for (const auto& line : output)
+        logln(line);
+    });
+
+    connect(m_cli, &QProcess::finished, this, [&]() {
+      logln("Finished");
+      delete m_cli;
+      m_cli = nullptr;
+    });
+
+    m_cli->start(program_path, arguments);
+  });
+
+  m_cancel_button = new QPushButton(m_main_buttons);
+  m_cancel_button->setText("&Cancel");
+  m_cancel_button->setFixedSize(button_size);
+  connect(m_cancel_button, &QPushButton::released, this, [&]() {
+    if (!m_cli) {
+      debugln("Not running");
+      return;
+    }
+    m_cli->kill();
+    logln(tr("Cancelled"));
+
+    m_cli = nullptr;
+  });
+
+  
+  m_main_buttons->layout()->addWidget(m_start_button);
+  m_main_buttons->layout()->addWidget(m_cancel_button);
+
+  m_main_controls->layout()->addWidget(m_path_selects);
+  m_main_controls->layout()->addWidget(m_main_buttons);
 
   m_settings_box = new QGroupBox(m_main_view);
 
@@ -46,7 +117,7 @@ MainWindow::MainWindow(QWidget *parent)
 #endif
 
   m_main_view->setLayout(new QVBoxLayout);
-  m_main_view->layout()->addWidget(m_path_selects);
+  m_main_view->layout()->addWidget(m_main_controls);
   m_main_view->layout()->addWidget(m_settings_box);
   m_main_view->layout()->addWidget(m_message_log);
   setCentralWidget(m_main_view);
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -4,6 +4,8 @@
 #include <QMainWindow>
 #include <QPointer>
 #include <QGroupBox>
+#include <QProcess>
+#include <QPushButton>
 #include <QTextEdit>
 
 #include "messagelog.h"
@@ -18,11 +20,18 @@ public:
   ~MainWindow();
 
 private:
+  QPointer<QProcess> m_cli = nullptr;
+
   QPointer<QWidget> m_main_view = nullptr;
 
-  QPointer<QGroupBox> m_path_selects = nullptr;
+  QPointer<QGroupBox> m_main_controls = nullptr;
+  QPointer<QWidget> m_path_selects = nullptr;
   QPointer<PathPicker> m_input_select = nullptr;
   QPointer<PathPicker> m_output_select = nullptr;
+
+  QPointer<QWidget> m_main_buttons = nullptr;
+  QPointer<QPushButton> m_start_button = nullptr;
+  QPointer<QPushButton> m_cancel_button = nullptr;
 
   QPointer<QGroupBox> m_settings_box = nullptr;
 
