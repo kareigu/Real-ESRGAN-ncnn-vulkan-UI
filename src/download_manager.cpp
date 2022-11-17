075417a223616ec516324bd4c7944a412641591d
karei <eetu.kivelae@gmail.com>
2022-11-17 11:59:01 +0200

    Implemented writing and extracing of downloaded zip file
    Downloaded zip file gets written to a temporary file on disk,
    where it's then handed over to elzip for extraction.
    After extraction the temporary file is removed.
    
    Plenty to clean up and improve here, just the first draft.

--- src/download_manager.cpp
+++ src/download_manager.cpp
@@ -1,16 +1,14 @@
 #include "download_manager.h"
 
 #include "message_log.h"
+#include "options.h"
+#include <QDir>
+#include <QFile>
 #include <QNetworkRequest>
+#include <elzip/elzip.hpp>
 
 DownloadManager::DownloadManager(const QString& fetch_url, QObject* parent) : m_fetch_url(fetch_url), QObject(parent) {
   connect(&m_network_manager, &QNetworkAccessManager::finished, this, &DownloadManager::download_finished);
-}
-
-void DownloadManager::download_finished(QNetworkReply* reply) {
-  m_downloaded_bytes = reply->readAll();
-  reply->deleteLater();
-  emit download_complete();
 }
 
 void DownloadManager::start_download() {
@@ -22,6 +20,38 @@ void DownloadManager::start_download() {
     logln(info);
   });
 }
+
+void DownloadManager::download_finished(QNetworkReply* reply) {
+  m_downloaded_bytes = reply->readAll();
+  reply->deleteLater();
+
+  auto cli_folder = Options::cli_location();
+  if (!QDir(cli_folder).exists())
+    QDir().mkpath(cli_folder);
+
+  constexpr auto temp_zip_filename = "cli.zip";
+  QFile zip_file(cli_folder + "/" + temp_zip_filename);
+
+  if (!zip_file.open(QIODevice::WriteOnly)) {
+    auto error_info = QString("Couldn't open temporary zip file for writing - path = %1/%2").arg(cli_folder, temp_zip_filename);
+    logln(error_info);
+    return;
+  }
+
+  QDataStream output(&zip_file);
+  output.writeRawData(m_downloaded_bytes, static_cast<int>(m_downloaded_bytes.length()));
+
+  zip_file.close();
+  debugln("Wrote temporary zip file");
+
+  elz::extractZip(zip_file.filesystemFileName(), cli_folder.toStdString());
+  debugln("Extracted temporary zip file");
+  if (zip_file.remove())
+    debugln("Removed temporary zip file");
+
+  emit download_complete();
+}
+
 void DownloadManager::handle_error(QNetworkReply::NetworkError error) {
   auto error_info = QString("Error while downloading files: %1").arg(error);
   logln(error_info);
